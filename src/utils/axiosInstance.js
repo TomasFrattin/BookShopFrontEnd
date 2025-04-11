5a3af4702451078f307cda0ec4f1398e989b8c89
TomasFrattin <120804091+TomasFrattin@users.noreply.github.com>
2025-04-11 09:40:49 -0300

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
-// Creamos una instancia de axios para hacer las solicitudes
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,  // URL pública de tu backend
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 
-// Interceptor para agregar el token a las cabeceras antes de cada solicitud
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
 
-    // Si el token está presente, lo agregamos a las cabeceras de la solicitud
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
